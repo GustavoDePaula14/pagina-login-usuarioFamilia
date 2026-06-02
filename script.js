90cc1119cb4deb3ce7a2efbfbf218808a788821a
Gustavo de Paula Silva <gustavodepaulasilva299@gmail.com>
2026-06-02 14:28:00 -0300

    Fix token formatting in URL construction

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const urlParams = new URLSearchParams(queryString);
 const token = urlParams.get('token')
 console.log(token)
 
-const url = `https://tcc-back-q3kw.onrender.com/v1/familysync/usuario-familia/emailEnviado?token=${token}`
+const url = `https://tcc-back-q3kw.onrender.com/v1/familysync/usuario-familia/emailEnviado?token='${token}'`
 console.log(url)
 const response = await fetch(url, {
     method: 'POST', // ou 'PUT', dependendo do seu backend
