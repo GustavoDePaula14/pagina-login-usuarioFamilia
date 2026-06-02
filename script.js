6cee5c2def8dc566e18ad60b8bfdd5e05b7497db
GUSTAVO DE PAULA SILVA <25132581@dsn127m1242315.cfp127aluno.local>
2026-06-02 13:42:12 -0300

    teste

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ const queryString = window.location.search;
 
 const urlParams = new URLSearchParams(queryString);
 
-const to_email = urlParams.get('email'); // Retorna "123"
-const id = urlParams.get('id_familia'); // Retorna "eletronicos"
-
-const juntarUsuarioFamilia = async function(to_email, id) {
-    const url = `https://tcc-back-q3kw.onrender.com/v1/familysync/usuario-familia/emailEnviado?email=${to_email}&id_familia=${id}`
+const token = urlParams.get('token')
+console.log(token)
+const juntarUsuarioFamilia = async function(token) {
+    const url = `https://tcc-back-q3kw.onrender.com/v1/familysync/usuario-familia/emailEnviado?token=${token}`
     const response = await fetch(url)
     const data = await response.json()
     return data
 }
-const resultado = await juntarUsuarioFamilia(to_email, id)
-return resultado
+juntarUsuarioFamilia(token)
